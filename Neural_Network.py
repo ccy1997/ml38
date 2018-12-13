8f1c34583dafb72c179b85b328d0dbb0ffc72223
axm1461 <45556626+axm1461@users.noreply.github.com>
2018-12-13 13:24:10 +0000

    Add files via upload

---
+++ Neural_Network.py
@@ -0,0 +1,105 @@
+import pandas
+import numpy as np
+from sklearn import preprocessing
+from scipy import stats
+from sklearn.model_selection import KFold
+import random
+import DataPrepUtil
+import Impute
+from sklearn.neural_network import MLPRegressor
+from sklearn.model_selection import GridSearchCV
+
+
+housing = pandas.read_csv('housing.csv')
+
+
+DataPrepUtil.transform_ocean_proximity(housing)
+Impute.fill_lr_prediction_from_other_column(housing, 'total_rooms')
+
+preprocessing.StandardScaler().fit(housing)
+df = standard.transform(housing)
+
+median_house_value_bc, maxlog, interval = stats.boxcox(housing.median_house_value, alpha=0.05)
+population_bc, maxlog, interval = stats.boxcox(housing.population, alpha=0.05)
+housing_median_age_bc, maxlog, interval = stats.boxcox(housing.housing_median_age, alpha=0.05)
+total_rooms_bc, maxlog, interval = stats.boxcox(housing.total_rooms, alpha=0.05)
+total_bedrooms_bc, maxlog, interval = stats.boxcox(housing.total_bedrooms, alpha=0.05)
+households_bc, maxlog, interval = stats.boxcox(housing.households, alpha=0.05)
+median_income_bc, maxlog, interval = stats.boxcox(housing.median_income, alpha=0.05)
+
+housing_boxcox = housing.copy()
+
+housing_boxcox.drop(columns=['housing_median_age'], inplace=True)
+housing_boxcox.drop(columns=['total_rooms'], inplace=True)
+housing_boxcox.drop(columns=['total_bedrooms'], inplace=True)
+housing_boxcox.drop(columns=['population'], inplace=True)
+housing_boxcox.drop(columns=['households'], inplace=True)
+housing_boxcox.drop(columns=['median_income'], inplace=True)
+housing_boxcox.drop(columns=['median_house_value'], inplace=True)
+
+housing_boxcox['housing_median_age'] = housing_median_age_bc
+housing_boxcox['total_rooms'] = total_rooms_bc
+housing_boxcox['total_bedrooms'] = total_bedrooms_bc
+housing_boxcox['population'] = population_bc
+housing_boxcox['households'] = households_bc
+housing_boxcox['median_income'] = median_income_bc
+housing_boxcox['median_house_value'] = median_house_value_bc
+
+y = housing_boxcox.median_house_value.values.reshape(-1,1)
+X = housing_boxcox.drop(columns=['median_house_value'], inplace=False).values
+
+print(X.shape)
+print(y.shape)
+# Pull out 1000 values into a holdout set
+holdout = random.sample(range(0,10640),1000)
+X_holdout = X[holdout]
+y_holdout = y[holdout]
+Xt = np.delete(X, holdout, 0)
+yt = np.delete(y, holdout, 0)
+print(Xt.shape)
+print(yt.shape)
+
+train_R2_average = 0
+test_R2_average = 0
+mae_average = 0
+accuracy_average = 0
+
+fold_number = 10
+
+kf = KFold(n_splits=fold_number, shuffle=True)
+
+
+Model = MLPRegressor(hidden_layer_sizes=(20,),
+                                       activation='relu',
+                                       solver='adam',
+                                       learning_rate='adaptive',
+                                       max_iter=500,
+                                       learning_rate_init=0.005,
+                                       early_stopping=True,
+                                       validation_fraction=0.1)
+
+# Finding optimal regularisation parameter (alpha)
+
+alphas = np.array([1, 0.1, 0.01, 0.001, 0])
+
+fold_counter = 1
+
+for train_index, test_index in kf.split(Xt): 
+    X_train, X_test = Xt[train_index], Xt[test_index]
+    y_train, y_test = yt[train_index], yt[test_index]
+      
+    
+    grid = GridSearchCV(estimator=Model, param_grid=dict(alpha=alphas), cv=3)
+    
+    grid.fit(X_train, y_train.ravel()) 
+    
+    train_R2_average += grid.score(X_train, y_train)/fold_number
+    test_R2_average += grid.score(X_test, y_test)/fold_number
+    
+    print('fold ', fold_counter, ' of ', fold_number, ' completed')
+    fold_counter += 1
+
+print('Mean CV score of best alpha: ', grid.best_score_)
+print('Best alpha value: ', grid.best_estimator_.alpha)
+print('Average training R^2 score: ', train_R2_average)
+print('Average test R^2 score: ', test_R2_average)
---
+++ normalisation.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Andrew Milton
+"""
+
+import pandas
+import numpy as np
+from sklearn.model_selection import KFold
+import random
+import sklearn.linear_model as lm
+from sklearn import preprocessing
+import DataPrepUtil
+import Impute
+
+
+housing = pandas.read_csv('housing.csv')
+
+DataPrepUtil.transform_ocean_proximity(housing)
+Impute.fill_lr_prediction_from_other_column(housing, 'total_rooms')
+
+StandardScaler = preprocessing.StandardScaler()
+MaxAbsScaler = preprocessing.MaxAbsScaler()
+MinMaxScaler = preprocessing.MinMaxScaler()
+RobustScaler = preprocessing.RobustScaler()
+
+scaled_1 = pandas.DataFrame(preprocessing.StandardScaler().fit_transform(housing))
+scaled_2 = pandas.DataFrame(preprocessing.MaxAbsScaler().fit_transform(housing))
+scaled_3 = pandas.DataFrame(preprocessing.MinMaxScaler().fit_transform(housing))
+scaled_4 = pandas.DataFrame(preprocessing.RobustScaler().fit_transform(housing))
+
+scaled_1.columns = housing.columns
+scaled_2.columns = housing.columns
+scaled_3.columns = housing.columns
+scaled_4.columns = housing.columns
+
+scaler_names = ['StandardScaler', 'MaxAbsScaler', 'MinMaxScaler', 'RobustScaler'] 
+
+scaler_count = 0
+
+for i in (scaled_1, scaled_2, scaled_3, scaled_4): 
+    
+ 
+    housing = i
+    
+    y = housing.median_house_value.values.reshape(-1,1)
+    X = housing.drop(columns=['median_house_value'], inplace=False).values
+    
+    holdout = random.sample(range(0,10640),1000)
+    X_holdout = X[holdout]
+    y_holdout = y[holdout]
+    Xt = np.delete(X, holdout, 0)
+    yt = np.delete(y, holdout, 0)
+    
+    fold_number = 10
+    
+    kf = KFold(n_splits=fold_number, shuffle=True)
+    
+    Model = lm.LinearRegression()
+
+    train_R2_average = 0
+    test_R2_average = 0
+    mae_average = 0
+    accuracy_average = 0
+        
+    for train_index, test_index in kf.split(Xt): 
+        X_train, X_test = Xt[train_index], Xt[test_index]
+        y_train, y_test = yt[train_index], yt[test_index]
+        
+        Model.fit(X_train, y_train.ravel()) 
+        
+        
+        train_R2_average += Model.score(X_train, y_train)/fold_number
+        test_R2_average += Model.score(X_test, y_test)/fold_number
+
+        
+    print('Average training R^2 score for ', scaler_names[scaler_count], ': ', train_R2_average)
+    
+    print('Average test R^2 scorefor ', scaler_names[scaler_count], ': ', test_R2_average, '\n')
+    scaler_count += 1
+    
